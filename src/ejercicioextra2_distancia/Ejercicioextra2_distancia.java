81429430408aa7d57f33bde25e0b2a98c951ab23
W4nD-0rDz <wandaordonez@gmail.com>
2023-03-27 18:04:53 -0300

    primer commit

---
+++ src/ejercicioextra2_distancia/Ejercicioextra2_distancia.java
@@ -0,0 +1,24 @@
+
+package ejercicioextra2_distancia;
+
+import Entidad.Punto;
+
+/**
+ *
+ * @author Wendy
+ */
+public class Ejercicioextra2_distancia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Punto par =new Punto();
+        par.crearPuntos();
+        
+       par.imprimir();
+       
+        
+    }
+    
+}
